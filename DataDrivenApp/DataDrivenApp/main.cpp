f6f9920cb71e7b7e427e64b2e4c9f9c81e4a66ae
Simron Kaur <simron.kaur17@bathspa.ac.uk>
2019-04-11 16:56:27 +0100

    Project setup

---
+++ DataDrivenApp/DataDrivenApp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DataDrivenApp
+//
+//  Created by Simron Kaur on 11/04/2019.
+//  Copyright © 2019 Simron Kaur. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
